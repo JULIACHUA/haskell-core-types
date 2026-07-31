942f2bfb5a3616bfa624f24b56beb1d128daa67d
JULIACHUA <juliachua00@gmail.com>
2026-07-31 20:48:22 +0000

    chore: sync cabal module state definitions

--- src/Types.hs
+++ src/Types.hs
@@ -7,3 +7,9 @@ data MetricRecord_26478 = MetricRecord_1282
   { nodeId_32201 :: !Int
   , isActive_27876 :: !Bool
   } deriving (Eq, Show)
+
+-- | Node state representation 10187
+data ClusterVector_13357 = ClusterVector_31873
+  { nodeId_8037 :: !Int
+  , isActive_10286 :: !Bool
+  } deriving (Eq, Show)
