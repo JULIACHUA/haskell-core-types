b19eaedaea4d06bb0d82faf8e4fc31453de14ece
JULIACHUA <juliachua00@gmail.com>
2026-07-31 15:54:47 +0000

    fix: adjust record field mappings in Haskell schema

---
+++ src/Types.hs
@@ -0,0 +1,9 @@
+module Types where
+
+-- Core High-Assurance Data Definitions
+
+-- | Node state representation 3340
+data MetricRecord_26478 = MetricRecord_1282
+  { nodeId_32201 :: !Int
+  , isActive_27876 :: !Bool
+  } deriving (Eq, Show)
